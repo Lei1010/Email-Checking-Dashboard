4fa88539889fdd3b9ca9104f633f7c081747a652
Leing <lienguyen1010@gmail.com>
2022-09-02 15:01:54 +0700

    first-graph

--- Main.py
+++ Main.py
@@ -11,38 +11,46 @@ import plotly.graph_objects as go
 import plotly.express as px
 
 
-def total_by_date(df):
-    fig = go.Figure(data=go.Scatter(x=df['RECEIVED']
-                                    , y=df['count_mail']))
+def total_by_date(df_total, df_null):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df_total['received_date'],
+                             y=df_total['count_mail'],
+                             name='Total Mail'))
+
+    fig.add_trace(go.Scatter(x=df_null['received_date'],
+                             y=df_null['count_mail'],
+                             name='Null Subject Mail',
+                             mode='markers'))
+
     return fig
 
 
-def main():
-    raw_report_df = pd.read_csv(r'D:\Downloads\CWNZ01.EML_REPORT_20220624.csv', header=0)
-    raw_report_df.drop_duplicates(['RECEIVED', 'SUBJECT', 'FROM', 'TO', 'NO_OF_ATTACHMENTS', 'BODY_CHARACTER_COUNT'],
-                                  inplace=True)
-    raw_report_df = raw_report_df[~raw_report_df.FILENAME.str.contains('neml', case=False)]
+raw_report_df = pd.read_csv(r'D:\Downloads\CWNZ01.EML_REPORT_20220624.csv', header=0)
+raw_report_df.drop_duplicates(['RECEIVED', 'SUBJECT', 'FROM', 'TO', 'NO_OF_ATTACHMENTS', 'BODY_CHARACTER_COUNT'],
+                              inplace=True)
+raw_report_df = raw_report_df[~raw_report_df.FILENAME.str.contains('neml', case=False)]
 
-    raw_report_df['received_date'] = pd.to_datetime(raw_report_df.RECEIVED).dt.date
-    raw_report_df['month'] = pd.to_datetime(raw_report_df.RECEIVED).dt.month
+raw_report_df['received_date'] = pd.to_datetime(raw_report_df.RECEIVED).dt.date
+raw_report_df['month'] = pd.to_datetime(raw_report_df.RECEIVED).dt.month
 
-    print(raw_report_df.head(5))
+print(raw_report_df.head(5))
 
-    count_date = raw_report_df.groupby('received_date', as_index=False).agg(
-        count_mail=pd.NamedAgg(column='PATH', aggfunc='count'))
+total_mail = raw_report_df.groupby('received_date', as_index=False).agg(
+    count_mail=pd.NamedAgg(column='PATH', aggfunc='count'))
 
-    app = dash.Dash()
-    app.layout = html.Div(id='parent',
-                          children=[
-                              html.H1(id='H1', children='Checking Email Report', style={'textAlign': 'center',
-                                                                                        'marginTop': 40,
-                                                                                        'marginBottom': 40}),
-                              dcc.Graph(id='line_plot', figure=total_by_date(raw_report_df))
-                          ]
-                          )
+null_subject = raw_report_df[~raw_report_df.SUBJECT.notna()].groupby('received_date', as_index=False).agg(
+    count_mail=pd.NamedAgg(column='PATH', aggfunc='count')
+)
 
-    app.run_server(debug=True)
-
+app = dash.Dash()
+app.layout = html.Div(id='parent',
+                      children=[
+                          html.H1(id='H1', children='Checking Email Report', style={'textAlign': 'center',
+                                                                                    'marginTop': 40,
+                                                                                    'marginBottom': 40}),
+                          dcc.Graph(id='total_by_date', figure=total_by_date(total_mail, null_subject))
+                      ]
+                      )
 
 if __name__ == '__main__':
-    main()
+    app.run_server(debug=True)
